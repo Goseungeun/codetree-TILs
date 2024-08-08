c671c70107f39502bf7f4428c5155af020c228c5
Goseungeun <74888933+Goseungeun@users.noreply.github.com>
2024-08-08 23:34:19 +0900

    Title: 수열 뒤집기, Time: 5040ms, Memory: 30MB, Status: Time Limit Exceed - CodeTree

--- 240808/수열 뒤집기/invert-sequence.py
+++ 240808/수열 뒤집기/invert-sequence.py
@@ -5,7 +5,7 @@ B1,B2 = map(int,input().split())
 num_list = [i + 1 for i in range(N)]
 
 for _ in range(K):
-    num_list = num_list[:A1-1] + num_list[A2-1:A1-2:-1] + num_list[A2:]
-    num_list = num_list[:B1-1] + num_list[B2-1:B1-2:-1] + num_list[B2:]
+    num_list = num_list[:A1-1] + list(reversed(num_list[A1-1:A2])) + num_list[A2:]
+    num_list = num_list[:B1-1] + list(reversed(num_list[B1-1:B2]))+ num_list[B2:]
 
 print('\n'.join(list(map(str,num_list))))
