63220e57196d26eaaee6d6eb7e7f2c39a1b1ee3e
Goseungeun <74888933+Goseungeun@users.noreply.github.com>
2024-08-12 22:07:00 +0900

    Title: 개발자의 공놀이, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240812/개발자의 공놀이/developer's-ball-game.py
+++ 240812/개발자의 공놀이/developer's-ball-game.py
@@ -3,12 +3,16 @@ programers = list(map(int,input().split()))
 programers.sort(reverse = True)
 
 ball = 1
+flag = True
 
 for i in range(1,n-1):
     left_dist = programers[i-1] - programers[i]
     right_dist = programers[i] - programers[i+1]
 
-    if left_dist < right_dist:
+    if left_dist < right_dist and flag:
         ball += 1
+        flag = False
+    elif left_dist >= right_dist:
+        flag = True
 
 print(ball)
