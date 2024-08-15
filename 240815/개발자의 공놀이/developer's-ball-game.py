6e7bed1a760b811ab34fd1ca92629ac5789a4bd6
Goseungeun <74888933+Goseungeun@users.noreply.github.com>
2024-08-15 23:11:38 +0900

    Title: 개발자의 공놀이, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240815/개발자의 공놀이/developer's-ball-game.py
@@ -0,0 +1,21 @@
+n = int(input())
+programers = list(map(int,input().split()))
+programers.sort(reverse = True)
+
+next_pro = [0] * n
+in_degree = [0] * n
+ball = 0
+
+next_pro[0] = in_degree[0] = 1
+next_pro[n-1] , in_degree[n-2] = n-2, 1
+
+for i in range(1,n-1):
+    nxt = i+1 if programers[i] - programers[i+1] <= programers[i-1] - programers[i] else i-1
+    next_pro[i] = nxt
+    in_degree[nxt] += 1
+
+for j in range(n):
+    if (in_degree[j] == 0 or (next_pro[next_pro[j]] == j and in_degree[j] == 1 and in_degree[next_pro[j]] == 1 and j < next_pro[j])):
+        ball += 1
+
+print(ball)
